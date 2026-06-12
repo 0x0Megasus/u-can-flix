c369cc6591a1daf5b966514f611ef912adc369b7
0x0Megasus <0x00megasus@gmail.com>
2026-06-12 15:15:56 +0100

    Fix ads provider script

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,7 +40,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
-        <script dangerouslySetInnerHTML={{__html: "(function(){var s=[document.documentElement,document.body].filter(Boolean).pop().appendChild(document.createElement('script'));s.dataset.zone='11133077';s.src='https://nap5k.com/tag.min.js';})();"}} />
+        {/* <script src="https://quge5.com/88/tag.min.js" data-zone="248494" async data-cfasync="false"></script> */}
+        <script src="https://quge5.com/88/tag.min.js" data-zone="11129891" async data-cfasync="false"></script>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
